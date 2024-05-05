eaa41452e2970d4d174e207d886fbf71447807db
jaim13 <jaimmartinez13@gmail.com>
2024-05-04 18:21:28 -0600

    GameBoard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialGameBoard = [
+  [null, "X", "O"],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, col, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
